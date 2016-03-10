4873f257e11b813b52562c19b652c55e6d91c92d
sbecot <simon.becot@orange.com>
2016-03-10 16:03:40 +0100

    Update server.js

--- broker/BrokerServer/server.js
+++ broker/BrokerServer/server.js
@@ -1,3 +1,4 @@
+ 
 /**
  ******************************************************************************
  * @b Project : ReThink
@@ -6,12 +7,20 @@
  *
  ******************************************************************************
  *
- *                       Copyright (C) Orange Labs
+ *                       Copyright (C) 2016 Orange Labs
  *                       Orange Labs - PROPRIETARY
  *
- *       Disclosure to third parties or reproduction in any form what-
- *       soever, without prior written consent, is strictly forbidden
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. *
  ******************************************************************************
  *
  * @brief Entry point of the Broker server
